b9223e7c1a1be01e66f7bea5720408b0c106c4d3
Mondre <engelbrechtmondre@gmail.com>
2022-07-15 21:56:39 +0800

    Remove color pickers from html and js

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 const color = document.getElementById('color');
-const colorPickerPen = document.getElementById('color-picker-pen');
-const colorPickerBackground = document.getElementById('color-picker-background');
 const rainbow = document.getElementById('rainbow');
-const background = document.getElementById('background');
-const eraser = document.getElementById('eraser');
 const clear = document.getElementById('clear');
-const gridSize = document.getElementById('grid-size');
+const changeSize = document.getElementById('change-size');
 const sketchPad = document.getElementById('pad');
 
-function changeBackgroundColor() {
-    let newBackgroundColor = colorPickerBackground.value;
-    sketchPad.style.backgroundColor = newBackgroundColor;
-}
