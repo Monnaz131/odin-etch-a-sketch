f8550636751e2983e1e24a940a71071ef1f9f650
Mondre <engelbrechtmondre@gmail.com>
2022-07-15 22:49:03 +0800

    Add style to buttons and add default grid

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const clear = document.getElementById('clear');
 const changeSize = document.getElementById('change-size');
 const sketchPad = document.getElementById('pad');
 
+function createGrid(rows, cols) {
+    sketchPad.style.setProperty('--grid-rows', rows);
+    sketchPad.style.setProperty('--grid-cols', cols);
+    for (let i = 0; i < (rows * cols); i++) {
+        let cell = document.createElement("div");
+        sketchPad.appendChild(cell).className = "grid-item";
+    };
+};
+
+createGrid(16, 16);
